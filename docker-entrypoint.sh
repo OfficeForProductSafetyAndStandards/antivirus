ea8c4652ddddf703335139f91b23fc64d9e40a54
Steve Lorek <steve@stevelorek.com>
2021-07-15 14:50:55 +0100

    Fix errors running ClamAV without virus signature files downloaded beforehand

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,7 +4,10 @@ set -ex
 # Ensure all gems are installed.
 bundle check || bundle install
 
-# Start clamav
+# Download initial virus signature files which are required before starting the ClamAV daemon
+freshclam
+
+# Start ClamAV and keep virus signature files up to date
 freshclam -d &
 clamd &
 
